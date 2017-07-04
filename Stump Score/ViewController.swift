c6d6b450d4142c1a93c78e006a84483833ee206d
Tom Harrington <tph@atomicbird.com>
2017-07-04 15:07:27 -0600

    Add cheesy animations

--- Stump Score/ViewController.swift
+++ Stump Score/ViewController.swift
@@ -13,16 +13,33 @@ class ViewController: UIViewController {
     @IBOutlet weak var panelScoreLabel: UILabel!
     @IBOutlet weak var audienceScoreLabel: UILabel!
     
+    private var pendingPanelWorkItem : DispatchWorkItem?
+    private var pendingAudienceWorkItem : DispatchWorkItem?
+    
     var panelScore = 0 {
         didSet {
             UserDefaults.standard.set(panelScore, forKey: panelScoreKey)
             panelScoreLabel.text = String(panelScore)
+            
+            pendingPanelWorkItem?.cancel()
+            let workItem = DispatchWorkItem {
+                self.animateNewText(label: self.panelScoreLabel)
+            }
+            pendingPanelWorkItem = workItem
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000), execute:workItem) 
         }
     }
     var audienceScore = 0 {
         didSet {
             UserDefaults.standard.set(audienceScore, forKey: audienceScoreKey)
             audienceScoreLabel.text = String(audienceScore)
+
+            pendingAudienceWorkItem?.cancel()
+            let workItem = DispatchWorkItem {
+                self.animateNewText(label: self.audienceScoreLabel)
+            }
+            pendingAudienceWorkItem = workItem
+            DispatchQueue.main.asyncAfter(deadline: .now() + .milliseconds(1000), execute:workItem) 
         }
     }
     
@@ -48,6 +65,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func animateNewText(label:UILabel) -> Void {
+        UIView.animate(withDuration: 0.3, 
+                       animations: { 
+                        label.transform = CGAffineTransform.identity.rotated(by: CGFloat.pi).scaledBy(x: 2, y: 2)
+                        label.textColor = UIColor.red
+                        //                        self.panelScoreLabel.alpha = 0.0
+        }) { (_) in
+            label.transform = CGAffineTransform.identity
+            label.textColor = UIColor.white
+            //                                    self.panelScoreLabel.alpha = 1.0
+        }
+    }
+    
     @IBAction func raisePanelScore(_ sender: AnyObject) {
         panelScore += 1
     }
